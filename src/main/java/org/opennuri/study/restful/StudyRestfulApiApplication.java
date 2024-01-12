c84fed72349fe745ef130d9d3c46975d7189076e
kyoungiklee <kyoungik.lee+1@gmail.com>
2024-01-12 11:31:28 +0900

    First inital commit

---
+++ src/main/java/org/opennuri/study/restful/StudyRestfulApiApplication.java
@@ -0,0 +1,13 @@
+package org.opennuri.study.restful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyRestfulApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyRestfulApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/opennuri/study/restful/StudyRestfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.opennuri.study.restful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyRestfulApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
